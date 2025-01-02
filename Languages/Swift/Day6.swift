432a8a8f396bc01bb0f3d277611f29a412475081
ianramy <imwagore@gmail.com>
2025-01-02 18:31:34 +0300

    Day 6 of Languages

---
+++ Languages/Swift/Day6.swift
@@ -0,0 +1,10 @@
+// Languages - Swift - Day 6
+// Foundational Concepts: Basics, syntax, loops, conditionals, functions, and variables.
+
+// Write a program that initializes an array of integers, iterates through it, and prints each element.
+
+let numbers = [1, 2, 3, 4, 5]
+
+for number in numbers {
+    print(number)
+}
